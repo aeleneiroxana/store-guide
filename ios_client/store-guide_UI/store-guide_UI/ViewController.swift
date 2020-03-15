059cf306a74e9f12fd0536a138912f73350c9304
grigoreandreea <grigore.andreea.e@gmail.com>
2020-03-15 21:38:47 +0200

    Added ios-project

---
+++ ios_client/store-guide_UI/store-guide_UI/ViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ViewController.swift
+//  store-guide_UI
+//
+//  Created by Grigore Andreea on 15/03/2020.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDelegate, UITableViewDataSource {
+
+    var recordinSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
+    
+    var numberOfRecords:Int = 0
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    @IBOutlet weak var myTableView: UITableView!
+    
+    @IBAction func record(_ sender: Any) {
+        
+        //Check if we have an active recorder
+        if audioRecorder == nil {
+            numberOfRecords += 1
+            let fileName = getDirectory().appendingPathComponent("\(numberOfRecords).m4a")
+            
+            let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC), AVSampleRateKey: 12000, AVNumberOfChannelsKey: 1, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
+        
+            //Start audio recording
+            do {
+                audioRecorder = try AVAudioRecorder(url: fileName, settings: settings)
+                audioRecorder.delegate = self
+                audioRecorder.record()
+                
+                buttonLabel.setTitle("Stop Recording", for: .normal)
+            }
+            catch {
+                displayAlert(title: "Ups!", message: "Recording failed!")
+            }
+        }
+        else {
+            
+            //Stopping audio recording
+            audioRecorder.stop()
+            audioRecorder = nil
+            
+            UserDefaults.standard.set(numberOfRecords, forKey: "myNumber")
+            myTableView.reloadData()
+            
+            buttonLabel.setTitle("Start Recording", for: .normal)
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //Setting up session
+        recordinSession = AVAudioSession.sharedInstance()
+        
+        if let number:Int = UserDefaults.standard.object(forKey: "myNumber") as? Int {
+            numberOfRecords = number
+        }
+ 
+        AVAudioSession.sharedInstance().requestRecordPermission { (hasPermission) in
+            if hasPermission{
+                print("Accepted!")
+            }
+            
+        }
+    }
+    
+    //Function that gets path to directory
+    func getDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = paths[0]
+        return documentDirectory
+    }
+
+
+    //Function that display an alert
+    func displayAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "dismiss", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //SETTING UP TABLE VIEW//
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRecords
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row + 1)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let path = getDirectory().appendingPathComponent("\(indexPath.row + 1).m4a")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: path)
+            audioPlayer.play()
+        }
+        catch {
+            
+        }
+    }
+}
+
