166b20b5442fd8aa1e846e1cbac8d44fd2754eb2
aeleneiroxana <47813871+aeleneiroxana@users.noreply.github.com>
2020-06-07 16:28:20 +0300

    made app sort of work

---
+++ ios-live-supplier/live-supplier/GameVC.swift
@@ -0,0 +1,16 @@
+//
+//  GameVC.swift
+//  live-supplier
+//
+//  Created by Roxana Aelenei on 07/06/2020.
+//  Copyright © 2020 Andreea Grigore. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class GameVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
