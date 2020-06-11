16609c1fff8a066d0f1b362f97e5cc39bf02c690
Tavi <taviatwork@gmail.com>
2020-06-11 20:06:17 +0300

    Get rid of useless code

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 
 const liderboardRoutes = require('./api/routes/liderboards');
 
-app.use('/liderboards', liderboardRoutes);
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It works!'
+    });
+});
 
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: 'It works!'
-//     });
-// });
+app.use('/liderboards', liderboardRoutes);
 
 module.exports = app;
